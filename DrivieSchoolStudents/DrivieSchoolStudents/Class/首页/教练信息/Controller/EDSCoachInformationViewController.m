6f4ec790fa2baf75dc25136b1b5c562fcb7417e4
班文政 <1204470409@qq.com>
2018-08-28 23:28:20 +0800

    提交代码

--- DrivieSchoolStudents/DrivieSchoolStudents/Class/首页/教练信息/Controller/EDSCoachInformationViewController.m
+++ DrivieSchoolStudents/DrivieSchoolStudents/Class/首页/教练信息/Controller/EDSCoachInformationViewController.m
@@ -9,6 +9,7 @@
 #import "EDSCoachInformationViewController.h"
 
 #import "EDSCoachInformationHeaderView.h"
+#import "EDSCoachInformationFooterView.h"
 
 @interface EDSCoachInformationViewController ()<UITableViewDelegate,UITableViewDataSource>
 
@@ -21,13 +22,34 @@
     
     self.navigationItem.title = @"教练信息";
     
+    UIButton *determineBtn = [[UIButton alloc] init];
+    [determineBtn setTitle:@"确认预约" forState:UIControlStateNormal];
+    [determineBtn setTitleColor:WhiteColor forState:UIControlStateNormal];
+    determineBtn.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    determineBtn.backgroundColor = ThemeColor;
+    determineBtn.layer.cornerRadius = 5;
+    determineBtn.layer.masksToBounds = YES;
+    [self.view addSubview:determineBtn];
+    [determineBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(15);
+        make.right.mas_equalTo(-15);
+        make.height.mas_equalTo(45);
+        make.bottom.mas_equalTo(-18);
+    }];
+    
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     self.tableView.separatorColor = ClearColor;
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.top.mas_equalTo(0);
+        make.bottom.mas_equalTo(determineBtn.mas_top).mas_equalTo(-15);
+    }];
     
     EDSCoachInformationHeaderView *headerView = [[EDSCoachInformationHeaderView alloc] init];
     headerView.wz_size = CGSizeMake(kScreenWidth, 100);
     self.tableView.tableHeaderView = headerView;
+    
+    
 }
 
 #pragma mark ------------------------ tableView --------------------------------
@@ -41,25 +63,25 @@
     return 1;
 }
 
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // 1.确定重用标示:
-    static NSString *ID = @"UITableViewCell";
+    static NSString *ID = @"EDSCoachInformationFooterView";
     // 2.从缓存池中取
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    EDSCoachInformationFooterView *cell = [tableView dequeueReusableCellWithIdentifier:ID];
     // 3.如果空就手动创建
     if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        cell = [[EDSCoachInformationFooterView alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
     }
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     
     return cell;
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+- (CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 44;
+    return UITableViewAutomaticDimension;
 }
+
 
 @end
---
+++ DrivieSchoolStudents/DrivieSchoolStudents/Class/首页/教练信息/View/EDSCoachInformationFooterView.h
@@ -0,0 +1,13 @@
+//
+//  EDSCoachInformationFooterView.h
+//  DrivieSchoolStudents
+//
+//  Created by 班文政 on 2018/8/28.
+//  Copyright © 2018年 班文政. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDSCoachInformationFooterView : UITableViewCell
+
+@end
